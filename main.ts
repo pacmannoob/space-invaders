e84254c590005a5e75c1e06f65fc59a4c818ff86
pacmannoob <112730140+pacmannoob@users.noreply.github.com>
2022-09-02 14:42:01 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,117 @@
+game.splash("Spike spikeston saves the universe")
+info.setLife(3)
+let spike_spikeston = sprites.create(img`
+    . . . . . . c c c c c c c . . . 
+    . . . . . c f f 6 6 f f 7 c . . 
+    . . . . c 7 6 6 6 6 6 6 7 6 c . 
+    . . . c 7 7 7 7 7 7 7 7 7 7 c . 
+    . . . c 7 8 1 f f 1 6 7 7 7 c . 
+    . . . f 6 f 1 f f 1 f 7 7 7 f . 
+    . . . f 6 f 2 2 2 2 f 7 7 7 f . 
+    . . c c 6 f 2 2 2 2 f 7 7 6 f . 
+    . c 7 7 7 7 2 2 2 2 7 7 f c . . 
+    c 7 1 1 1 7 7 7 7 7 c c 7 7 c . 
+    f 1 1 1 1 1 7 7 7 f c 6 7 7 7 c 
+    f 1 1 1 1 1 1 6 f c c 6 6 6 c c 
+    f 6 1 1 1 1 1 6 6 c 6 6 6 c . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+    . f 6 1 1 1 1 6 6 6 6 6 c . . . 
+    . . f f c c c c c c c c . . . . 
+    `, SpriteKind.Player)
+spike_spikeston.setPosition(72, 99)
+controller.moveSprite(spike_spikeston, 90, 0)
+let list = [
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . c c c c . . 
+    . c c c c c . c c c c c f c c . 
+    c c a c c c c c 8 f f c f f c c 
+    c a f a a c c a f f c a a f f c 
+    c a 8 f a a c a c c c a a a a c 
+    c b c f a a a a a c c c c c c c 
+    c b b a a c f 8 a c c c 8 c c c 
+    . c b b a b c f a a a 8 8 c c . 
+    . . . . a a b b b a a 8 a c . . 
+    . . . . c b c a a c c b . . . . 
+    . . . . b b c c a b b a . . . . 
+    . . . . b b a b a 6 a . . . . . 
+    . . . . c b b b 6 6 c . . . . . 
+    . . . . . c a 6 6 b c . . . . . 
+    . . . . . . . c c c . . . . . . 
+    `,
+img`
+    . . . . . . . . . . b b b . . . 
+    . . . . . . . . b e e 3 3 b . . 
+    . . . . . . b b e 3 2 e 3 a . . 
+    . . . . b b 3 3 e 2 2 e 3 3 a . 
+    . . b b 3 3 3 3 3 e e 3 3 3 a . 
+    b b 3 3 3 3 3 3 3 3 3 3 3 3 3 a 
+    b 3 3 3 d d d d 3 3 3 3 3 d d a 
+    b b b b b b b 3 d d d d d d 3 a 
+    b d 5 5 5 5 d b b b a a a a a a 
+    b 3 d d 5 5 5 5 5 5 5 d d d d a 
+    b 3 3 3 3 3 3 d 5 5 5 d d d d a 
+    b 3 d 5 5 5 3 3 3 3 3 3 b b b a 
+    b b b 3 d 5 5 5 5 5 5 5 d d b a 
+    . . . b b b 3 d 5 5 5 5 d d 3 a 
+    . . . . . . b b b b 3 d d d b a 
+    . . . . . . . . . . b b b a a . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . c c . . . . . . 
+    . . . . . c a a a a . . . . . . 
+    . . . . . a a f f b a . . . . . 
+    . . . . c a b f f c b . . . . . 
+    . . . . c b b b a f c b . . . . 
+    . . . . c b a c a b b b . . . . 
+    . . . . . b b f f a a c . . . . 
+    . . . . . . a a b b c . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . c c c a c . . . . 
+    . . c c b b b a c a a a c . . . 
+    . c c a b a c b a a a b c c . . 
+    . c a b c f f f b a b b b a . . 
+    . c a c f f f 8 a b b b b b a . 
+    . c a 8 f f 8 c a b b b b b a . 
+    c c c a c c c c a b c f a b c c 
+    c c a a a c c c a c f f c b b a 
+    c c a b 6 a c c a f f c c b b a 
+    c a b c 8 6 c c a a a b b c b c 
+    c a c f f a c c a f a c c c b . 
+    c a 8 f c c b a f f c b c c c . 
+    . c b c c c c b f c a b b a c . 
+    . . a b b b b b b b b b b b c . 
+    . . . c c c c b b b b b c c . . 
+    . . . . . . . . c b b c . . . . 
+    `,
+img`
+    . . . . . . . e e e e . . . . . 
+    . . . . . e e 4 5 5 5 e e . . . 
+    . . . . e 4 5 6 2 2 7 6 6 e . . 
+    . . . e 5 6 6 7 2 2 6 4 4 4 e . 
+    . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
+    . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
+    . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
+    e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
+    e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
+    e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
+    e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
+    e 5 e c 5 4 5 4 5 5 5 e e . . . 
+    e 5 e e 5 5 5 5 5 4 e . . . . . 
+    4 5 4 e 5 5 5 5 e e . . . . . . 
+    . 4 5 4 5 5 4 e . . . . . . . . 
+    . . 4 4 e e e . . . . . . . . . 
+    `
+]
+game.onUpdateInterval(500, function () {
+    pause(100)
+})
